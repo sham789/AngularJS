b8e8f944e74dd47f1ad4eb3dd3d1196f66f0ccd3
Shamil Asiimov <shamfama@gmail.com>
2018-02-11 21:27:24 +0300

    sorting by name added

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -19,14 +19,45 @@ phone.controller('phoneList', function ($scope) {
     $scope.title = 'Смартфоны';
     $scope.phones = [
         {'name': 'Apple iPhone 8 64Gb Space Gray',
-            'snippet':'Faster stronger'},
+            'snippet':'Faster stronger',
+              'status': 'В наличии',
+                'priority': '4'},
         {'name': 'Apple iPhone 7 64Gb White',
-            'snippet': 'Faster stronger'},
+            'snippet': 'Faster stronger',
+              'status': 'В наличии',
+                'priority': '3'},
         {'name': 'Apple iPhone 6 64Gb Space Gray',
-            'snippet': 'Faster stronger'},
+            'snippet': 'Faster stronger',
+              'status': 'Нет на складе',
+                'priority': '2'},
         {'name': 'Apple iPhone 5S 64Gb White',
-            'snippet': 'Faster stronger'}
+            'snippet': 'Faster stronger',
+              'status': 'В наличии',
+                'priority': '1'}
     ];
     var date = new Date();
     $scope.today = date;
+
+    $scope.doneFilter = function(phoneItem) {
+      return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === 'В наличии';
+    }
+
+    $scope.sortField = undefined;
+    $scope.reverse = false;
+    $scope.sort = function(sname) {
+      if ($scope.sortField === sname) {
+        $scope.reverse = !$scope.reverse;
+      } else {
+        $scope.sortField = sname;
+        $scope.reverse = false;
+      };
+    };
+
+    $scope.isSortUp = function(sname) {
+      return $scope.sortField === sname && !$scope.reverse;
+    }
+    $scope.isSortDown = function(sname) {
+      return $scope.sortField === sname && $scope.reverse;
+    }
+
 });
